aa2f113c74eca2e1feb12230e6e567ff47d5710a
pingles <paul@oobaloo.co.uk>
2009-11-04 15:59:58 +0000

    Add load_data example

---
+++ examples/load_data.rb
@@ -0,0 +1,33 @@
+require File.join(File.dirname(__FILE__), *%w[.. lib rbhive])
+
+# CREATE TABLE:
+# CREATE TABLE keyword_reports (line_date STRING, acctname STRING, campaign STRING, adgroup STRING, keyword STRING, adDistributionWithSearchPartners STRING, kwDestUrl STRING, currCode STRING, imps INT, clicks INT, cost INT, pos DOUBLE)
+# PARTITIONED BY (import_date STRING, country STRING)
+# ROW FORMAT DELIMITED 
+#    FIELDS TERMINATED BY ',' 
+#    LINES TERMINATED BY '\n'
+# STORED AS TEXTFILE
+
+countries = %w(at br au de es fr it nl no pl ru se uk us)
+
+RBHive.connect('hadoopmaster.cluster.trafficbroker.co.uk') do |db|
+  
+  countries.each do |country|
+
+    (1..31).each do |day|
+      padded_day = day.to_s.rjust(2, '0')
+      
+      file_date = "200910#{padded_day}"
+      input_file = "/user/deploy/input/keyword-reports/#{country}-#{file_date}.csv"
+      
+      import_date = "2009-10-#{padded_day}"
+      command = "LOAD DATA INPATH '#{input_file}' INTO TABLE keyword_reports PARTITION (import_date='#{import_date}', country='#{country}')"
+      
+      puts "Running #{command}"
+      
+      db.execute(command)
+    end
+    
+  end
+  
+end
