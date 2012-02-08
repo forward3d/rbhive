0ca59653b29e6de11049196eecb53fd9375c8079
Andy Kent <andy.kent@me.com>
2012-02-08 10:53:46 +0000

    support for serde properties in table_schema

--- lib/rbhive/table_schema.rb
+++ lib/rbhive/table_schema.rb
@@ -10,45 +10,74 @@ module RBHive
       @collection_sep = options[:collection_sep] || "|"
       @columns = []
       @partitions = []
+      @serde_name = nil
+      @serde_properties = {}
       instance_eval(&blk) if blk
     end
-  
+
     def column(name, type, comment=nil)
       @columns << Column.new(name, type, comment)
     end
-  
+
     def partition(name, type, comment=nil)
       @partitions << Column.new(name, type, comment)
     end
-  
+
+    def serde(name, properties={})
+      @serde_name = name
+      @serde_properties = properties
+    end
+
     def create_table_statement()
       %[CREATE #{external}TABLE #{table_statement}
-  ROW FORMAT DELIMITED
-  FIELDS TERMINATED BY '#{@field_sep}'
-  COLLECTION ITEMS TERMINATED BY '#{@collection_sep}'
-  LINES TERMINATED BY '#{@line_sep}'
+  ROW FORMAT #{row_format_statement}
   STORED AS TEXTFILE
   #{location}]
     end
-  
+
+    def row_format_statement
+      if @serde_name
+        serde_statement
+      else
+        delimited_statement
+      end
+    end
+
+    def delimited_statement
+      %(DELIMITED
+  FIELDS TERMINATED BY '#{@field_sep}'
+  COLLECTION ITEMS TERMINATED BY '#{@collection_sep}'
+  LINES TERMINATED BY '#{@line_sep}')
+    end
+
+    def serde_statement
+      %(SERDE '#{@serde_name}'\n#{serde_properties_statement})
+    end
+
+    def serde_properties_statement
+      return '' unless @serde_properties.any?
+      kvs = @serde_properties.map { |k,v| %("#{k}" = "#{v}") }.join(",\n")
+      %(WITH SERDEPROPERTIES (#{kvs}))
+    end
+
     def replace_columns_statement
       alter_columns_statement("REPLACE")
     end
-  
+
     def add_columns_statement
       alter_columns_statement("ADD")
     end
-  
+
     def to_s
       table_statement
     end
-  
+
     private
 
     def external
       @location.nil? ? '' : 'EXTERNAL '
     end
-  
+
     def table_statement
       comment_string = (@comment.nil? ? '' : " COMMENT '#{@comment}'")
       %[`#{@name}` #{column_statement}#{comment_string}\n#{partition_statement}]
@@ -57,28 +86,28 @@ module RBHive
     def location
       @location.nil? ? '' : "LOCATION '#{@location}'"
     end
-  
+
     def alter_columns_statement(add_or_replace)
       %[ALTER TABLE `#{name}` #{add_or_replace} COLUMNS #{column_statement}]
     end
-  
+
     def column_statement
       cols = @columns.join(",\n")
       "(\n#{cols}\n)"
     end
-  
+
     def partition_statement
       return "" if @partitions.nil? || @partitions.empty?
       cols = @partitions.join(",\n")
       "PARTITIONED BY (\n#{cols}\n)"
     end
-  
+
     class Column
       attr_reader :name, :type, :comment
       def initialize(name, type, comment=nil)
         @name, @type, @comment = name, type, comment
       end
-    
+
       def to_s
         comment_string = @comment.nil? ? '' : " COMMENT '#{@comment}'"
         "`#{@name}` #{@type.to_s.upcase}#{comment_string}"
