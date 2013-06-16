3eb4b71c2542f204bcb5d1eca9911bf741b8149f
Andy Sykes <github@tinycat.co.uk>
2013-06-16 15:09:07 +0100

    Add create table / drop table example for HiveServer2

--- examples/hive2-demo.rb
+++ examples/hive2-demo.rb
@@ -5,3 +5,13 @@ require File.join(File.dirname(__FILE__), *%w[.. lib rbhive])
 RBHive.tcli_connect("hive.hadoop.forward3d.com", 10_000, nil) do |connection|
   puts connection.fetch("SHOW TABLES")
 end
+
+table = RBHive::TableSchema.new('testing', 'Things') do
+  column 'foo', :int, 'Value'
+  column 'bar', :string, 'String value'
+end
+
+RBHive.tcli_connect("hive.hadoop.forward3d.com", 10_000, nil) do |connection|
+  p connection.create_table(table)
+  p connection.drop_table(table)
+end
