445512ab769156382ea68d80932ca59c9160a5ff
Andy Sykes <github@tinycat.co.uk>
2013-10-23 14:55:59 +0100

    Remove old examples, since they're in the README now

--- examples/hive2-demo.rb
+++
@@ -1,5 +0,0 @@
-require File.join(File.dirname(__FILE__), *%w[.. lib rbhive])
-
-RBHive.tcli_connect("hive.hadoop.forward3d.com", 10_002, :http, nil) do |connection|
-  puts connection.fetch("select count(*) from gucci_keywords where dated='2013-01-01'")
-end
