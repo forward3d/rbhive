5858f8d8ab99709fba9eeb436c09363a1f3d9993
Andy Sykes <github@tinycat.co.uk>
2014-03-28 16:58:46 +0000

    Do not coerce to nil when Hive returns "NULL" for a column

--- lib/rbhive/t_c_l_i_schema_definition.rb
+++ lib/rbhive/t_c_l_i_schema_definition.rb
@@ -67,7 +67,6 @@ module RBHive
       type = column_type_map[column_name]
       return 1.0/0.0 if(type != :string && value == "Infinity")
       return 0.0/0.0 if(type != :string && value == "NaN")
-      return nil if value.nil? || value == 'NULL' || value == 'null'
       return coerce_complex_value(value) if type.to_s =~ /^array/
       conversion_method = TYPES[type]
       conversion_method ? value.send(conversion_method) : value
