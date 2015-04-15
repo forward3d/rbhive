f7a5c3b3392a577851bc081e0bd5b63ca89cc487
Graham Jackson <graham.jackson07@gmail.com>
2015-04-15 12:18:46 -0400

    Correct spelling of #to_separated_output

--- lib/rbhive/result_set.rb
+++ lib/rbhive/result_set.rb
@@ -4,30 +4,30 @@ module RBHive
       @schema = schema
       super(rows.map {|r| @schema.coerce_row(r) })
     end
-    
+
     def column_names
       @schema.column_names
     end
-    
+
     def column_type_map
       @schema.column_type_map
     end
-    
+
     def to_csv(out_file=nil)
-      to_seperated_output(",", out_file)
+      to_separated_output(",", out_file)
     end
-    
+
     def to_tsv(out_file=nil)
-      to_seperated_output("\t", out_file)
+      to_separated_output("\t", out_file)
     end
-    
+
     def as_arrays
       @as_arrays ||= self.map{ |r| @schema.coerce_row_to_array(r) }
     end
-    
+
     private
-    
-    def to_seperated_output(sep, out_file)
+
+    def to_separated_output(sep, out_file)
       rows = self.map { |r| @schema.coerce_row_to_array(r).join(sep) }
       sv = rows.join("\n")
       return sv if out_file.nil?
